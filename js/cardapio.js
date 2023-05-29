aadb4db6e725dc1a5096be5c8a89cc0481159255
Victor Lupi <victor.lupi@hotmail.com>
2023-05-29 15:52:14 -0300

    adicionado todas as categorias e templates de produtos

--- js/cardapio.js
+++ js/cardapio.js
@@ -1,68 +1,87 @@
 fetch('JSON/cardapio.json')
-.then(response => response.json())
-.then(data => {
-  const bolosContainer = document.getElementById('bolos-container');
-  const docesContainer = document.getElementById('doces-container');
-  const items = data.items;
+  .then(response => response.json())
+  .then(data => {
+    const bolosBrancos = document.getElementById('bolos-brancos');
+    const bolosMorango = document.getElementById('bolos-morango');
+    const bolosChocolate = document.getElementById('bolos-chocolate');
+    const bolosNozes = document.getElementById('bolos-nozes');
+    const salgadosSimples = document.getElementById('salgados-simples');
+    const salgadosEspeciais = document.getElementById('salgados-especiais');
+    const docesSimples = document.getElementById('doces-simples');
+    const docesEspeciais = document.getElementById('doces-especiais');
+    const items = data.items;
 
-  
-  items.forEach(item => {
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('col-md-3', 'col-sm-6');
+    items.forEach(item => {
+      
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('col-md-3', 'col-sm-6');
+      
+      const cardDiv = document.createElement('div');
+      cardDiv.classList.add('card', 'text-bg-dark');
+      
+      const link = document.createElement('a');
+      link.href = item.link;
+      
+      const image = document.createElement('img');
+      image.src = item.image;
+      image.alt = 'Bolos';
+      image.classList.add('card-img', 'zoom-image');
+      image.loading = 'lazy';
+      
+      const overlayDiv = document.createElement('div');
+      overlayDiv.classList.add('card-img-overlay-notop');
+      
+      const title = document.createElement('h4');
+      title.classList.add('card-title');
+      title.innerHTML = item.title;
+      
+      const whatsappLink = 'https://api.whatsapp.com/send?text=' + encodeURIComponent(item.title);
+      item.link = 'https://api.whatsapp.com/send?text=' + encodeURIComponent(item.title);
 
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('card', 'text-bg-dark');
+      const description = document.createElement('p');
 
-    const link = document.createElement('a');
-    link.href = item.link;
-    
+      const orderLink = document.createElement('a');
+      orderLink.href = item.link;
+      orderLink.classList.add('btn', 'btn-second');
+      orderLink.textContent = 'Peça o seu';
 
-    const image = document.createElement('img');
-    image.src = item.image;
-    image.alt = 'Bolos';
-    image.classList.add('card-img', 'zoom-image');
-    image.loading = 'lazy';
+      overlayDiv.appendChild(title);
+      overlayDiv.appendChild(description);
+      overlayDiv.appendChild(orderLink);
 
-    const overlayDiv = document.createElement('div');
-    overlayDiv.classList.add('card-img-overlay-notop');
+      link.appendChild(image);
+      cardDiv.appendChild(link);
+      cardDiv.appendChild(overlayDiv);
 
-    const title = document.createElement('h4');
-    title.classList.add('card-title');
-    title.textContent = item.title;
+      itemDiv.appendChild(cardDiv);
 
-    const description = document.createElement('p');
-    
+      if (item.description === 'bolosBrancos') {
+        bolosBrancos.appendChild(itemDiv);
+      } else if (item.description === 'bolosMorango') {
+        bolosMorango.appendChild(itemDiv);
+      } else if (item.description === 'bolosChocolate') {
+        bolosChocolate.appendChild(itemDiv);
+      } else if (item.description === 'bolosNozes') {
+        bolosNozes.appendChild(itemDiv);
+      } else if (item.description === 'salgadosSimples') {
+        salgadosSimples.appendChild(itemDiv);
+      } else if (item.description === 'salgadosEspeciais') {
+        salgadosEspeciais.appendChild(itemDiv);
+      } else if (item.description === 'docesSimples') {
+        docesSimples.appendChild(itemDiv);
+      } else if (item.description === 'docesEspeciais') {
+        docesEspeciais.appendChild(itemDiv);
+      }
+    });
 
-    const orderLink = document.createElement('a');
-  
-    orderLink.href = item.link;
-    orderLink.classList.add('btn', 'btn-second');
-    orderLink.textContent = 'Peça o seu';
+    // Efeito parallax
+    var parallaxContainer = document.querySelector('.parallax-container');
 
-    overlayDiv.appendChild(title);
-    overlayDiv.appendChild(description);
-    overlayDiv.appendChild(orderLink);
-
-    link.appendChild(image);
-    cardDiv.appendChild(link);
-    cardDiv.appendChild(overlayDiv);
-
-    itemDiv.appendChild(cardDiv);
-
-    if (item.description === 'bolos') {
-      bolosContainer.appendChild(itemDiv);
-    } else if (item.description === 'doces') {
-      docesContainer.appendChild(itemDiv);
-    }
+    window.addEventListener('scroll', function() {
+      var scrollTop = window.pageYOffset;
+      parallaxContainer.style.backgroundPosition = 'center ' + (-scrollTop / 2) + 'px';
+    });
+  })
+  .catch(error => {
+    console.error('Erro ao carregar o arquivo JSON:', error);
   });
-})
-.catch(error => {
-  console.error('Erro ao carregar o arquivo JSON:', error);
-});
-
-var parallaxContainer = document.querySelector('.parallax-container');
-
-window.addEventListener('scroll', function() {
-  var scrollTop = window.pageYOffset;
-  parallaxContainer.style.backgroundPosition = 'center ' + (-scrollTop / 2) + 'px';
-});
